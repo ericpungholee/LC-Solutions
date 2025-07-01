aed5d0f353f531016c4a656e7c9db69b8fb1428e
Eric Lee <eric.pungho.lee@gmail.com>
2025-07-01 15:27:21 -0300

    Update Longest Consecutive Sequence.py

--- LC-Solutions/Medium/Longest Consecutive Sequence.py
+++ LC-Solutions/Medium/Longest Consecutive Sequence.py
@@ -1,25 +1,36 @@
-class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
+"""
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+You must write an algorithm that runs in O(n) time.
+"""
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #just return 0 when nums is empty
         if len(nums) == 0:
             return 0
 
+        #store all start of sequences
+        #turn nums into set to get rid of duplicates.
         start_candidates = []
         num_set = set(nums)
 
+        #n is start of a sequence only if there's no number that's less by 1
         for n in num_set:
             if n-1 not in num_set:
                 start_candidates.append(n)
 
         seq_lens = set()
-
+        
+        #add lengths of all sequences to a set and return maximum
         for s in start_candidates:
-            seq = [s]
             curr = s
-            for n in num_set:
-                if curr+1 in num_set:
-                    curr = curr+1
-                    seq.append(curr)
-            seq_lens.add(len(seq))
+            length = 1
+            while curr+1 in num_set:
+                length+=1
+                curr = curr+1
+            seq_lens.add(length)
         
         return max(seq_lens)
-            
